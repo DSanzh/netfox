f50004d968a8ac6635befad222156522cac40bbe
Sanzhar <dauylov.sanzhar@gmail.com>
2021-01-14 00:24:34 +0600

    add

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "netfox",
-            path: "Sources")
+            path: "Sources",
+            exclude: ["NFXLoader.h", "NFXLoader.m"])
     ]
 )
