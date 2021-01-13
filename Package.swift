7b5fb8bca8b286122641e591a8c9de652af2fc33
Sanzhar <dauylov.sanzhar@gmail.com>
2021-01-13 23:37:43 +0600

    asd

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "netfox",
-            exclude: ["Sources/Core/NFXLoader.h", "Sources/Core/NFXLoader.m"],
-            path: "Sources"),
+            exclude: ["Sources/Core/NFXLoader.h", "Sources/Core/NFXLoader.m"])
     ]
 )
