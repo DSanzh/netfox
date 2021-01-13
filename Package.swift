94fefa070db7c92ae1aeb90a81bed01711290b40
Sanzhar <dauylov.sanzhar@gmail.com>
2021-01-13 23:45:32 +0600

    fixes issues

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "netfox",
-            targets: ["NetFox"]),
+            targets: ["netfox"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "netfox",
+            path: "Sources",
             exclude: ["Sources/Core/NFXLoader.h", "Sources/Core/NFXLoader.m"])
     ]
 )
